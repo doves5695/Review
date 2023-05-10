d9c2079b3df84c438d2a066384c004298f28e0b8
jingrui <790150796@qq.com>
2023-05-10 15:43:38 +0800

    5.10

---
+++ JS/dist/4.15.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function mytest() {
+  console.log(666);
+}
+
+var obj = {
+  firstTest: function firstTest() {
+    console.log('666');
+  },
+  secondTest: function secondTest() {
+    console.log('777');
+  }
+};
